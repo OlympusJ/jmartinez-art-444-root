ee1c28a56aaee55a633fdcad20357c7a486a9bbe
OlympusJ <jmartinez8386@sdsu.edu>
2025-04-18 12:32:04 -0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -6,16 +6,16 @@ function revealMenu () {
 function closeMenu() {
     document.getElementById('nav').classList.remove('open');
 }
-let tothetop = document.getElementById("topbutton");
-window.onscroll = function() {scrollFunction()};
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      tothetop.style.display = "block";
-    } else {
-      tothetop.style.display = "none";
-    }
-  }
-  function topFunction() {
-    document.body.scrollTop = 0; 
-    document.documentElement.scrollTop = 0; 
-  }
+// let tothetop = document.getElementById("topbutton");
+// window.onscroll = function() {scrollFunction()};
+// function scrollFunction() {
+//     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//       tothetop.style.display = "block";
+//     } else {
+//       tothetop.style.display = "none";
+//     }
+//   }
+//   function topFunction() {
+//     document.body.scrollTop = 0; 
+//     document.documentElement.scrollTop = 0; 
+//   }
