8a45d44ce72a0a74f86dddf8fb31ebd7ea9d51a0
OlympusJ <jmartinez8386@sdsu.edu>
2025-04-18 12:17:16 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function revealMenu () {
+   var menu = document.getElementById("nav")
+   
+   menu.classList.toggle("open");
+}
+function closeMenu() {
+    document.getElementById('nav').classList.remove('open');
+}
+let tothetop = document.getElementById("topbutton");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      tothetop.style.display = "block";
+    } else {
+      tothetop.style.display = "none";
+    }
+  }
+  function topFunction() {
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0; 
+  }
